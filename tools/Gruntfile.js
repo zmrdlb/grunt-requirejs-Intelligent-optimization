52656259f874542dc8317db9d0d33ffd570a3ddd
zmrdlb <592044573@qq.com>
2015-09-15 14:52:18 +0800

    修复requirejs打包bug
    aa

--- tools/Gruntfile.js
+++ tools/Gruntfile.js
@@ -4,9 +4,20 @@ module.exports = function(grunt) {
 	 * 1. requirejs优化，最好不要用jquery的压缩后的插件，因为会重复压缩。实在不行配合参数skipDirOptimize: true，让不在modules里面的文件不进行优化
 	 */
 	//requirejs用户自定义扩展配置项，非官方标准配置。为了实现智能简便配置
+	/**
+	 * 关于配置项路径的写法有以下重要说明：
+	 * 1. mfolder里写成相对路径，基于appDir和baseUrl下
+	 * 2. 如果在paths中给路径设置了别名，exclude里的key(除了all)和value都写成别名，没有则写成正常的相对路径（类似于mfolder）
+	 */
 	var requirejsconfig = {
 		mfolder: ['page','page2'], //待打包的文件夹，默认此文件夹下的所有Js都打包。文件夹基于appDir和baseUrl下
 		modules: [{name:'config'}], //modules项初始化
+		paths: { //requirejs.config里面的paths。没有则为{}
+			'api1': 'third/api1',
+			'api2': 'third/api2',
+			'jquery.cookie': 'third/jquery.cookie',
+			'jquery': 'http://www.zmr.com/lib/js/core/jquery/jquery-1.11.3.min'
+		},
 		/**
 		* 给每个待打包的文件，指定不需要打包的文件.
 		* @param {name: ['file']} 打包name文件或文件夹下的所有文件，排除对指定file列表的文件的打包
@@ -17,8 +28,10 @@ module.exports = function(grunt) {
 		*	'page/main1': ['api1'] page/main1.js不打包模块依赖api1
 		* }
 		* 不需要此项，则为{}
-	    */
-	   /**
+		* 
+		* 注意：exclude指定的不打包的文件将会只打包直接依赖，不会打包直接依赖的依赖。
+		* 如api1依赖common/a，会打包common/a，但是common/a依赖的common/b不会被打包进来
+	    *
 	    *  如果shim配置项依赖项是cdn加载方式，那么build后会出错。具体原因可以参加requirejs官网“"shim"配置的优化器重要注意事项:”
 	    *  解决方案有以下几种（优先级从高到低排列）：
 	    * 	  1. 将shim配置项改成AMD写法，不需要shim配置
@@ -30,11 +43,64 @@ module.exports = function(grunt) {
 	
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		requirejs: {}
+		requirejs: {}		
 	});
 	
 	//配置requirejs的任务
 	grunt.task.registerTask('addfiles','config',function(){
+		/**
+		 * 数组中是否存在某值 
+		 */
+		Array.prototype.inArray = function(val){
+			var result = false;
+			for(var i = 0, len = this.length; i < len; i++){
+				if(this[i] == val){
+					result = true;
+					break;
+				}
+			}
+			return result;
+		};
+		
+		/**
+		 * 对于 requirejsconfig.paths别名首次匹配数据获取
+		 */
+		var pathskeys = []; //匹配requirejsconfig.paths中设置的别名的首次判断关键字
+		if(requirejsconfig.paths){
+			for(var name in requirejsconfig.paths){
+				var matcharr = requirejsconfig.paths[name].match(/([^\/]+)(\/|\b)/);
+				if(matcharr && matcharr[1] != 'http:' && !pathskeys.inArray(matcharr[1])){
+					pathskeys.push(matcharr[1]);
+				}
+			}
+			pathskeys = pathskeys.join('|');
+			console.log('关键字'+pathskeys);
+		}
+		
+		/**
+		 * 根据grunt file读取的完整路径返回处理后的filepath(处理包括只获取相对于baseUrl中的路径，和对于paths设置中有别名的返回别名)
+		 */
+		function getfilepath(filesrc){
+			var filepath = filesrc.match(/.*js\/(.*).js/)[1];
+			var patharr = filepath.split('/');
+			if(requirejsconfig.paths && pathskeys != ''){
+				var reg = new RegExp(pathskeys);
+				if(reg.test(patharr[0])){ //匹配首个关键字，则继续查找是否有别名
+					console.log('首个关键字匹配'+filesrc);
+					var change = false;
+					for(var name in requirejsconfig.paths){
+						var _fulpath = requirejsconfig.paths[name];
+						if(_fulpath.indexOf('http:') < 0){
+							var reg1 = new RegExp(_fulpath+'\\b');
+							filepath = filepath.replace(reg1,function(){change = true; return name;});
+							if(change){break;}
+						}
+					}
+				}
+			}
+			return filepath;
+		}
+		
 		/**
 		* 根据文件完整相对路径filesrc，从变量requirejsconfig.exclude中获取当前文件的完整exclude配置项
 		*/
@@ -59,18 +125,13 @@ module.exports = function(grunt) {
 			}
 			return result;
 		}
-		/**
-		* 根据grunt解析出来的路径，获取需要进行优化的Js的完整路径
-		*/
-		function getfullpath(file){
-			return file.match(/.*js\/(.*).js/)[1];
-		}
+		
 		//requirejs打包处理程序
 		for(var i = 0, len = requirejsconfig.mfolder.length; i < len; i++){
 			var folder = requirejsconfig.mfolder[i];
 			var files = grunt.file.expand('../web/js/'+folder+'/**/*.js');
 			files.forEach(function(file) {
-				var filesrc = getfullpath(file); //获取文件完整相对路径
+				var filesrc = getfilepath(file); //获取文件相对路径
 				var opt = {
 					name: filesrc
 				};
@@ -81,6 +142,7 @@ module.exports = function(grunt) {
 				requirejsconfig.modules.push(opt);
 			});
 		}
+		console.log(requirejsconfig.modules);
 		grunt.config.set('requirejs',{
 			compile: { //requirejs官方标准配置
 				options: {
@@ -98,12 +160,7 @@ module.exports = function(grunt) {
 					//fileExclusionRegExp: /common|comp/, 
 					//stubModules: ['api1'], //这个配置项有问题，api1压缩后正常，但是在打包后的main1.js里内容被清空define("api1",{})
 					//配置文件中相关路径的配置项
-					paths: {
-						'api1': 'third/api1',
-						'api2': 'third/api2',
-						'jquery.cookie': 'third/jquery.cookie',
-						'jquery': 'http://www.zmr.com/lib/js/core/jquery/jquery-1.11.3.min'
-					},
+					paths: requirejsconfig.paths,
 					shim: {
 						'jquery.cookie': ['jquery']
 					},
